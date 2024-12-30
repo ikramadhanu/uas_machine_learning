c8eb7ced859c7f09b52f2f5419a8546f5597e2f8
Gantriser <ilhamramadhanu25@gmail.com>
2024-12-30 09:14:00 +0700

    add fish predict

--- app.py
+++ app.py
@@ -19,7 +19,38 @@ if (selected=="SUPPORT VECTOR MACHINE"):
     w_l_ratio = st.number_input("w_l_ratio")
     fish = st.button("Predict Fish")
     if fish:
-        st.success('PREDICTION : ')
+        with open('Model/svm_fish.pickle', "rb") as r:
+            svc = pickle.load(r)
+        new_data = {
+            'length' : [length],
+            'weight' : [weight],
+            'w_l_ratio' : [w_l_ratio],
+        }
+        new_data = pd.DataFrame(new_data)
+        scaler = StandardScaler()
+        scaled_new_data = scaler.fit_transform(new_data)
+        new_prediction = svc.predict(new_data)
+
+        if new_prediction[0] == 0 :
+            hasil = 'Anabas testudineus'
+        elif new_prediction[0] == 1 :
+            hasil = 'Coilia dussumieri'
+        elif new_prediction[0] == 2 :
+            hasil = 'Otolithoides biauritus'
+        elif new_prediction[0] == 3 :
+            hasil = 'Otolithoides pama'
+        elif new_prediction[0] == 4 :
+            hasil = 'Pethia conchonius'
+        elif new_prediction[0] == 5 :
+            hasil = 'Polynemus paradiseus'
+        elif new_prediction[0] == 6 :
+            hasil = 'Puntius lateristriga'
+        elif new_prediction[0] == 7 :
+            hasil = 'Setipinna taty'
+        elif new_prediction[0] == 8 :
+            hasil = 'Sillaginopsis panijus'
+        
+        st.success('PREDICTION : {}'.format(hasil))
     st.divider()
 
     st.title('PUMPKIN CLASSIFICATION')
@@ -81,7 +112,38 @@ if (selected=="PERCEPTRON"):
     w_l_ratio = st.number_input("w_l_ratio")
     fish = st.button("Predict Fish")
     if fish:
-        st.success('PREDICTION : ')
+        with open('Model/perceptron_fish.pickle', "rb") as r:
+            perceptron = pickle.load(r)
+        new_data = {
+            'length' : [length],
+            'weight' : [weight],
+            'w_l_ratio' : [w_l_ratio],
+        }
+        new_data = pd.DataFrame(new_data)
+        scaler = StandardScaler()
+        scaled_new_data = scaler.fit_transform(new_data)
+        new_prediction = perceptron.predict(new_data)
+
+        if new_prediction[0] == 0 :
+            hasil = 'Anabas testudineus'
+        elif new_prediction[0] == 1 :
+            hasil = 'Coilia dussumieri'
+        elif new_prediction[0] == 2 :
+            hasil = 'Otolithoides biauritus'
+        elif new_prediction[0] == 3 :
+            hasil = 'Otolithoides pama'
+        elif new_prediction[0] == 4 :
+            hasil = 'Pethia conchonius'
+        elif new_prediction[0] == 5 :
+            hasil = 'Polynemus paradiseus'
+        elif new_prediction[0] == 6 :
+            hasil = 'Puntius lateristriga'
+        elif new_prediction[0] == 7 :
+            hasil = 'Setipinna taty'
+        elif new_prediction[0] == 8 :
+            hasil = 'Sillaginopsis panijus'
+        
+        st.success('PREDICTION : {}'.format(hasil))
     st.divider()
 
     st.title('PUMPKIN CLASSIFICATION')
@@ -143,7 +205,38 @@ if (selected=="LOGISTIC REGRESSION"):
     w_l_ratio = st.number_input("w_l_ratio")
     fish = st.button("Predict Fish")
     if fish:
-        st.success('PREDICTION : ')
+        with open('Model/logistic_fish.pickle', "rb") as r:
+            log = pickle.load(r)
+        new_data = {
+            'length' : [length],
+            'weight' : [weight],
+            'w_l_ratio' : [w_l_ratio],
+        }
+        new_data = pd.DataFrame(new_data)
+        scaler = StandardScaler()
+        scaled_new_data = scaler.fit_transform(new_data)
+        new_prediction = log.predict(new_data)
+
+        if new_prediction[0] == 0 :
+            hasil = 'Anabas testudineus'
+        elif new_prediction[0] == 1 :
+            hasil = 'Coilia dussumieri'
+        elif new_prediction[0] == 2 :
+            hasil = 'Otolithoides biauritus'
+        elif new_prediction[0] == 3 :
+            hasil = 'Otolithoides pama'
+        elif new_prediction[0] == 4 :
+            hasil = 'Pethia conchonius'
+        elif new_prediction[0] == 5 :
+            hasil = 'Polynemus paradiseus'
+        elif new_prediction[0] == 6 :
+            hasil = 'Puntius lateristriga'
+        elif new_prediction[0] == 7 :
+            hasil = 'Setipinna taty'
+        elif new_prediction[0] == 8 :
+            hasil = 'Sillaginopsis panijus'
+        
+        st.success('PREDICTION : {}'.format(hasil))
     st.divider()
 
     st.title('PUMPKIN CLASSIFICATION')
